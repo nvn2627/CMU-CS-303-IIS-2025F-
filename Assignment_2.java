5972dc64859ac4471f463acb13a5f663d3d605c1
Nguyen Van Nguyen <nvn2627@gmail.com>
2025-08-14 10:46:46 +0700

    init project

---
+++ Assignment_2.java
@@ -0,0 +1,25 @@
+
+package javaassignments;
+
+
+public class Assignment_2 {
+    public static void main(String[] args)
+    {
+        int a = 9, b = 5;
+        System.out.println("a / b = " + a / b);
+        
+        System.out.println("695 % 20 = " + 695 % 20);
+        
+        System.out.println("7 + 6 * 5 = " + (7 + 6 * 5));
+        
+        System.out.println("7 * 6 + 5 = " + (7 * 6 + 5));
+        
+        System.out.println("248 % 100 / 5 = " + (248 % 100 / 5));
+        
+        System.out.println("6 * 3 - 9 / 4 = " + (6 * 3 - 9 / 4));
+        
+        System.out.println("(5 - 7) * 4 = " + (5 - 7) * 4);
+        
+        System.out.println("6 + (18 % (17 - 12)) = " + (6 + (18 % (17 - 12))));
+    }
+}
