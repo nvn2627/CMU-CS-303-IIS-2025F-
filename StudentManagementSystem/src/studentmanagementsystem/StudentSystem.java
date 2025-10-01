d7984469b6017d8af38e8c559252a92f0565c0f3
Nguyễn Văn Nguyên <139242660+nvn2627@users.noreply.github.com>
2025-10-01 12:38:51 +0700

    Individual project

---
+++ StudentManagementSystem/src/studentmanagementsystem/StudentSystem.java
@@ -0,0 +1,10 @@
+package studentmanagementsystem;
+
+public class StudentSystem {
+
+    public static void main(String[] args) {
+        SFStudent st = new SFStudent();
+        st.show();
+    }
+
+}
